fe500673c48c9cd592fab781aafc094d9f046aea
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-03-02 16:26:24 -0500

    Añade una funcion que agrega divs de libros
    Esta funcion toma como argumento un array y agrega al booksContainer
    un div nuevo por cada elemento del array.
    
    Esta implmentacion no comprueba si el elemento ya esta agregado al
    booksContainer, siempre agrega una cantidad igual a la cantidad de
    elementos en el array.
    Cuando se deba utilizar esta funcion para modificar en vivo se debe
    hacer cambios para solucionar esto.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const booksContainer = document.querySelector(".booksContainer");
 
 function Book(title, autor, pageNum, wasRead) {
   this.title = title;
@@ -15,6 +16,14 @@ function AddBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function DisplayBooks(bookArray) {
+  const books = bookArray;
+  books.forEach((book) => {
+    const bookCard = document.createElement("div");
+    booksContainer.appendChild(bookCard);
+  });
+}
+
 const bookOne = new Book("Grandioso", "Samir", 295, "not read yet");
 AddBookToLibrary(bookOne);
-console.log(myLibrary[0]);
+DisplayBooks(myLibrary);
