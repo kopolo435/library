fe688d6da8fd8695ba83ac2ef5f83f2bcff54888
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-03-03 14:44:19 -0500

    Agrega una funcion que consigue datos del form
    Agrega variable que apunta al form y una variable con sus inputs
    La funcion no tiene parametros, devuelve un array con un valor para todos
    los inputs. Un valor de este array sera undefinde dependinedo de que radio
    button este seleccionado

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const addBookBtn = document.querySelector("#addBook");
 const formContainer = document.querySelector(".formContainer");
 const cancelBtn = document.querySelector("#cancelBtn");
 const saveBookBtn = document.querySelector("#saveBookBtn");
+
+const bookForm = document.querySelector("form");
+const bookInputs = bookForm.querySelectorAll("input");
+
 function Book(title, autor, pageNum, wasRead) {
   this.title = title;
   this.autor = autor;
@@ -38,6 +42,22 @@ function DisplayBooks(bookArray) {
   });
 }
 
+function GetInputsValues() {
+  let inputsValues = Array.from(bookInputs).map((input) => {
+    if (
+      input.getAttribute("type") === "text" ||
+      input.getAttribute("type") === "number"
+    ) {
+      return input.value;
+    }
+    //Comprueba input type="radio"
+    if (input.checked) {
+      return input.value;
+    }
+  });
+  return inputsValues;
+}
+
 addBookBtn.addEventListener("click", () => {
   formContainer.classList.add("showFormContainer");
 });
@@ -46,9 +66,10 @@ cancelBtn.addEventListener("click", () => {
   formContainer.classList.remove("showFormContainer");
 });
 
-saveBookBtn.addEventListener("click", Event =>{
-    Event.preventDefault();
-})
+saveBookBtn.addEventListener("click", (Event) => {
+  Event.preventDefault();
+  console.log(GetInputsValues());
+});
 
 const bookOne = new Book("Grandioso", "Samir", 295, "not read yet");
 const bookwTwo = new Book("Inmenso", "Samir", 100, "yes");
