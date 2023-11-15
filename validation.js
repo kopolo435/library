bb150c732b3b70fb8cb1353233f1dda616c06b55
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-11-14 21:23:52 -0500

    Arregla bugs de if al comprobar value

--- validation.js
+++ validation.js
@@ -3,15 +3,15 @@ function validateValues(){
     const author = document.getElementById("autor");
     const pageNum = document.getElementById("autor");
     let isValid = true;
-    if(!title.validity.valueMissing){
+    if(title.validity.valueMissing){
         title.setCustomValidity("Se espera agregue un titulo");
         isValid = false;
     }
-    if(!author.validity.valueMissing){
+    if(author.validity.valueMissing){
         author.setCustomValidity("Se espera agregue el autor del libro");
         isValid = false;
     }
-    if(!pageNum.validity.valueMissing){
+    if(pageNum.validity.valueMissing){
         pageNum.setCustomValidity("Se espera agregue la cantidad de paginas del libro");
         isValid = false;
     }
