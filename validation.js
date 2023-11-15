0038d494f11b0f9ecf6a8713f49f7fce789e0320
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-11-15 09:42:01 -0500

    Agrega mensajes especificos de error

--- validation.js
+++ validation.js
@@ -1,21 +1,46 @@
 function validateValues(){
     const title = document.getElementById("title");
     const author = document.getElementById("autor");
-    const pageNum = document.getElementById("autor");
-    let isValid = true;
+    const pageNum = document.getElementById("pgNum");
     if(title.validity.valueMissing){
         title.setCustomValidity("Se espera agregue un titulo");
-        isValid = false;
+    }else{
+        title.setCustomValidity("");
     }
     if(author.validity.valueMissing){
         author.setCustomValidity("Se espera agregue el autor del libro");
-        isValid = false;
+    }else{
+        author.setCustomValidity("");
     }
     if(pageNum.validity.valueMissing){
         pageNum.setCustomValidity("Se espera agregue la cantidad de paginas del libro");
-        isValid = false;
+    }else{
+        pageNum.setCustomValidity("");
     }
-    return isValid
+
+    title.addEventListener("input",()=>{
+        if(title.validity.valueMissing){
+            title.setCustomValidity("Se espera agregue un titulo");
+        }else{
+            title.setCustomValidity("");
+        }
+    })
+
+    author.addEventListener("input",()=>{
+        if(author.validity.valueMissing){
+            author.setCustomValidity("Se espera agregue el autor del libro");
+        }else{
+            author.setCustomValidity("");
+        }
+    })
+
+    pageNum.addEventListener("input",()=>{
+        if(pageNum.validity.valueMissing){
+            pageNum.setCustomValidity("Se espera agregue la cantidad de paginas del libro");
+        }else{
+            pageNum.setCustomValidity("");
+        }
+    })
 }
 
 export default validateValues ;
