167929eaabd960b141f16bd5c2da7006229769cf
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-11-14 21:07:53 -0500

    Agrega modulo para validar inputs

---
+++ validation.js
@@ -0,0 +1,21 @@
+function validateValues(){
+    const title = document.getElementById("title");
+    const author = document.getElementById("autor");
+    const pageNum = document.getElementById("autor");
+    let isValid = true;
+    if(!title.validity.valueMissing){
+        title.setCustomValidity("Se espera agregue un titulo");
+        isValid = false;
+    }
+    if(!author.validity.valueMissing){
+        author.setCustomValidity("Se espera agregue el autor del libro");
+        isValid = false;
+    }
+    if(!pageNum.validity.valueMissing){
+        pageNum.setCustomValidity("Se espera agregue la cantidad de paginas del libro");
+        isValid = false;
+    }
+    return isValid
+}
+
+export default validateValues ;
